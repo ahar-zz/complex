1a357432b28972cd1165aa37887691f9cdacdff2
renat <renat.kabirov@bookedin.net>
2019-01-17 11:56:40 -0600

    checkpoint

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var oldConsole = window.console.log;
-var console = {};
-console.log = function(e){
-    oldConsole("I'm here " + e);
-};
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
